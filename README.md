3aefc95aaa375b48466e5ff7ccc9e8498ebe5591
captchasolver <info@solvecaptcha.com>
2025-03-24 23:17:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# 🛡️ Cloudflare Bypass Demo
+
+## 📖 Overview
+
+This example illustrates how to bypass a Cloudflare Challenge page protected by Turnstile CAPTCHA using Puppeteer in combination with SolveCaptcha.
+
+> 📄 *You can learn more in the article [Bypassing Cloudflare Challenge with Puppeteer and SolveCaptcha](https://solvecaptcha.com/blog/bypassing-cloudflare-challenge-with-puppeteer-and-solvecaptcha) (coming soon).*
+
+## ⚙️ Getting Started
+
+Clone the repository, install the required packages, configure your environment, and run the script.
+
+```bash
+git clone git@github.com:solvecaptcha/cloudflare-demo.git
+cd cloudflare-demo
+export APIKEY=your_api_key
+yarn install
+yarn start
+```
+
+## 📜 Ethical Use Notice
+
+This repository is provided for **educational and research purposes only**.  
+Please use this code responsibly, and **never** apply it to websites without proper authorization.  
+Make sure your actions comply with local laws, terms of service, and good digital manners.
+
+If you're unsure — ask. If you're still unsure — don't do it. 🙃
+
+## 😅 P.S. Just in case...
+
+We know you're here just to test your own stuff.  
+You're probably an ethical hacker, QA engineer, or someone who's just really tired of clicking traffic lights.  
+We trust you. But the Cloudflare bot? Not so much. 🤖💥
+
+## 📄 License
+
+This project is licensed under the MIT License. For more information, refer to the [LICENSE](./LICENSE) file.
---
+++ index.js
@@ -0,0 +1,55 @@
+import { launch } from 'puppeteer'
+import { Solver } from 'solvecaptcha-javascript'
+import { readFileSync } from 'fs'
+import { normalizeUserAgent } from './normalize-ua.js'
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const solver = new Solver(process.env.APIKEY)
+
+const example = async () => {
+    // If you are using `headless: true` mode, you need to fix userAgent. NormalizeUserAgent is used for this purpose.
+    const initialUserAgent = await normalizeUserAgent()
+
+    const browser = await launch({
+        headless: false,
+        devtools: true,
+        args: [
+            `--user-agent=${initialUserAgent}`,
+        ]
+    })
+
+    const [page] = await browser.pages()
+
+    const preloadFile = readFileSync('./inject.js', 'utf8');
+    await page.evaluateOnNewDocument(preloadFile);
+
+
+    // Here we intercept the console messages to catch the message logged by inject.js script
+    page.on('console', async (msg) => {
+        const txt = msg.text()
+        if (txt.includes('intercepted-params:')) {
+            const params = JSON.parse(txt.replace('intercepted-params:', ''))
+            console.log(params)
+
+            try {
+                console.log(`Solving the captcha...`)
+                const res = await solver.cloudflareTurnstile(params)
+                console.log(`Solved the captcha ${res.id}`)
+                console.log(res)
+                await page.evaluate((token) => {
+                    cfCallback(token)
+                }, res.data)
+            } catch (e) {
+                console.log(e.err)
+                return process.exit()
+            }
+        } else {
+            return;
+        }
+    })
+    // Replace the target page address with your own
+    page.goto('https://solvecaptcha.com/demo/cloudflare-turnstile-challenge')
+}
+
+example()
